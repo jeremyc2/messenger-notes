c96507f5b20ef2cad22b0c3d283c3736d1c3ec73
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2022-02-21 07:27:27 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,11 @@ import { useEffect, useRef } from 'react';
 const Home: NextPage = () => {
   const mainInput = useRef<HTMLDivElement>(null)
   useEffect(() => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+
     mainInput.current?.focus()
   }, [mainInput])
   
