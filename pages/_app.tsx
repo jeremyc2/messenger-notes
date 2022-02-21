fb909085b4fd9ce8603f2ba3e6105b31d68c3e1b
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2022-02-20 23:26:59 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import 'open-props/style';
 import 'open-props/normalize';
-import '../styles/style';
+import '../styles/globals.scss';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
