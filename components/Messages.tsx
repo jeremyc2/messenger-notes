1a5298a9d6bf9740787b4a43816de0dd460ea115
Jeremy Chandler <goldenmeso@gmail.com>
2022-02-22 10:36:42 -0600

    scroll

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,12 +1,19 @@
 import styles from '../styles/messages.module.scss'
+import { useEffect, useRef } from 'react'
 
 interface Props {
     messages: string[]
 }
 
 export default function MessageInput({ messages }: Props) {
+    const messagesContainer = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        if(!messagesContainer.current) return
+        messagesContainer.current.scrollTop = messagesContainer.current.scrollHeight
+    })
+
     return (
-        <div className={styles.messages}>
+        <div ref={messagesContainer} className={styles.messages}>
             {messages.map((message, index) => {
                 return <div key={`message-${index}`} className={styles.message} dangerouslySetInnerHTML={{ __html: message }}></div>
             })}
