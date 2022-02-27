23e0b3e85e70c36f0ad8d24fdeb2c9b99cc97a30
Jeremy Chandler <goldenmeso@gmail.com>
2022-02-27 08:53:39 -0600

    fix whitespace

--- components/MessageInput.tsx
+++ components/MessageInput.tsx
@@ -43,7 +43,9 @@ export default function MessageInput({ messages, setMessages }: Props) {
     }
 
     function sendMessage() {
-        if(!editor.current || editor.current.innerText === '') return
+        if(!editor.current || /^\s+$/.test(editor.current.innerText)) return
+
+        debugger
 
         const newMessage = createMessage(editor.current)
         setMessages([...messages, newMessage])
