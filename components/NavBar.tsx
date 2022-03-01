b211293100cd2d171e23437dc612e99e0d908d2b
Jeremy Chandler <goldenmeso@gmail.com>
2022-03-01 12:01:53 -0600

    add back arrow

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -16,6 +16,11 @@ export default function NavBar({ collection, setMessages }: Props) {
     return (
         <div className={styles.navbar}>
             <div className={styles.title}>
+                <a href="/notes">
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="var(--text-2)">
+                        <path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+                    </svg>
+                </a>
                 <img src="/animal-avatars/antelope1.png" alt="" />
                 <div>{collection || ''}</div>
             </div>
