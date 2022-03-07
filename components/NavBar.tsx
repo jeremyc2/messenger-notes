001fa99efedbddadedd2e16692d185a2e6b48b1f
Jeremy Chandler <goldenmeso@gmail.com>
2022-03-07 14:27:03 -0600

    fix size

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -31,7 +31,7 @@ export default function NavBar({ collection, setMessages }: Props) {
                 <div>{collection || ''}</div>
             </div>
             <button onClick={deleteMessages}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="var(--text-2)">
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="var(--text-2)">
                     <path d="M0 0h24v24H0V0z" fill="none"/>
                     <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/>
                 </svg>
