7eeefdd0a57a3e889796bc0c2c32f10386c99c0d
Jeremy Chandler <goldenmeso@gmail.com>
2022-03-09 20:11:02 -0600

    modal designing

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,11 +1,11 @@
 import styles from './modal.module.scss'
 import { createPortal } from 'react-dom'
-import { ReactNode, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 interface Props {
     show?: boolean
     setModalOpen: Function
-    children?: ReactNode[]
+    children?: JSX.Element
 }
 
 export default function Modal({ show = false, setModalOpen, children }: Props) {
@@ -15,9 +15,13 @@ export default function Modal({ show = false, setModalOpen, children }: Props) {
         setMounted(true)
     }, [])
 
+    function createCollection() {
+        setModalOpen(false)
+    }
+
     if(!(mounted && show)) return null
-    return createPortal(<div className={show? styles.show: ''}>
-        <div className={styles.content} onClick={() => setModalOpen(false)}>
+    return createPortal(<div className={show? styles.show: ''} onClick={createCollection}>
+        <div className={styles.content} onClick={(e) => e.stopPropagation()}>
             {children}
         </div>
     </div>, document.body)
--- components/NavBar/NavbBar2/NavBar2.tsx
+++ components/NavBar/NavbBar2/NavBar2.tsx
@@ -24,7 +24,9 @@ export default function NavBar2({ title }: Props) {
                     <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
                 </svg>
             </button>
-            <Modal show={modalOpen} setModalOpen={setModalOpen}></Modal>
+            <Modal show={modalOpen} setModalOpen={setModalOpen}>
+                <input type="text" />
+            </Modal>
         </div>
     )
 }
