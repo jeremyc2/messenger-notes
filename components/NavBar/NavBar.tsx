35ea6d89e97132245f65bad2fcf9a68096330cf8
Jeremy Chandler <goldenmeso@gmail.com>
2022-03-09 23:08:27 -0600

    refactor

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -15,12 +15,12 @@ export default function NavBar({ collection, setMessages }: Props) {
 
         localStorage.removeItem(collection)
         setMessages([])
-        router.push('/notes')
+        router.push('/')
     }
     return (
         <div className={styles.navbar}>
             <div className={styles.title}>
-                <Link href="/notes">
+                <Link href="/">
                     <a>
                         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="var(--text-2)">
                             <path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
