ccbae6c0b5e09c62d2eb82fadd0fb13c528ec2bb
Jeremy Chandler <goldenmeso@gmail.com>
2022-03-13 17:18:36 -0500

    started collection utility

---
+++ scripts/collection.ts
@@ -0,0 +1,25 @@
+// TODO implement me
+import { MessageNode } from './message-conversion'
+
+export interface Collection {
+    name: string
+    icon: string
+    latest: string
+    messages: MessageNode[][]
+}
+
+export function createCollection(name: string) {
+
+}
+
+export function readColletion(name: string) {
+
+}
+
+export function updateCollection() {
+
+}
+
+export function deleteCollection(name: string) {
+
+}
