1060f63065dd5f3bb88a7a8b5d7a02a7b8f1f398
Jeremy Chandler <goldenmeso@gmail.com>
2022-03-13 18:01:30 -0500

    build collection

--- scripts/collection.ts
+++ scripts/collection.ts
@@ -1,25 +1,42 @@
 // TODO implement me
 import { MessageNode } from './message-conversion'
 
-export interface Collection {
+type CollectionMap = [string, string]
+
+interface Collection {
     name: string
-    icon: string
-    latest: string
-    messages: MessageNode[][]
+    icon?: string
+    latest?: string
+    messages?: MessageNode[][]  
 }
 
 export function createCollection(name: string) {
 
 }
 
-export function readColletion(name: string) {
+export function getCollection(name: string) {
+    const collection = localStorage.getItem(name)
+    if(!collection) return
 
+    return JSON.parse(collection)
 }
 
-export function updateCollection() {
+export function getCollections() {
+const collections = Object.entries(localStorage)
+    return collections
+        .filter(collection => collection[0] !== 'ally-supports-cache')
+        .map(([name, data]: CollectionMap) => {
+          return {name, latest: JSON.parse(data).latest}
+        })
+}
 
+export function updateCollection(params: Collection) {
+    
+    const newCollection = {}
+
+    localStorage.setItem(params.name, JSON.stringify(newCollection))
 }
 
 export function deleteCollection(name: string) {
-
+    localStorage.removeItem(name)
 }
